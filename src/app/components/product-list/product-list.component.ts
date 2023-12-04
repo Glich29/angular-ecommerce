61de9f0ebca7f039e9ab44e6d578cc945d608248
Glich29 <sbislava@gmail.com>
2023-12-05 01:02:26 +0300

    Init angular ecommerce app

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
+import { CommonModule, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [NgFor, CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+  providers: [ProductService]
+})
+export class ProductListComponent {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {
+
+  }
+
+  ngOnInit() {
+    this.listProducts();
+  }
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+}
