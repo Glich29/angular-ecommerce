737cd5f41723163d3ff2a4e449a1a4d2d254f026
Glich29 <sbislava@gmail.com>
2023-12-07 23:09:13 +0300

    Add base routes for catigories

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,21 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { Routes, provideRouter, withDebugTracing } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent}
+  {path: 'category', component: ProductListComponent}
+  {path: 'products', component: ProductListComponent}
+  {path: '', redirectTo: '/products', pathMatch: 'full'}
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+]
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideRouter(routes, withDebugTracing())
   ]
 };
 
