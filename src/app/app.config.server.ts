211a0ee65f9169bcac7972c199e5d39857d5ce40
Glich29 <sbislava@gmail.com>
2023-12-07 23:09:47 +0300

    Fix app config

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -5,10 +5,10 @@ import { Routes, provideRouter, withDebugTracing } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
-  {path: 'category/:id', component: ProductListComponent}
-  {path: 'category', component: ProductListComponent}
-  {path: 'products', component: ProductListComponent}
-  {path: '', redirectTo: '/products', pathMatch: 'full'}
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
   {path: '**', redirectTo: '/products', pathMatch: 'full'}
 ]
 
